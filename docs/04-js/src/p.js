7487af616c48fea51d847b806f89e595788c39f3
FAIZAN PATEL <68345709+FAIZAN101013@users.noreply.github.com>
2026-02-01 11:45:53 +0000

    html updateded1

--- docs/04-js/src/p.js
+++ docs/04-js/src/p.js
@@ -1,35 +1,47 @@
-let evenNums = []
-let oddNums = []
+// Original array of numbers
+let groupofNums = [1,2,3,4,5,67,8,9,0,4,2,35,6,566,7,7683,2,2,1312,3,55,6];
 
+// Arrays to store results (GLOBAL – they keep growing)
+let evenNums = [];
+let oddNums = [];
 
+// New array created using spread operator
+let groupofNums2 = [
+  ...groupofNums,
+  12,234,12343,5345,645,645,756,56,8,1234234,534,5134
+];
 
-let groupofNums2 = [...groupofNums , 12,234,12343,5345,645,645,756,56,8,1234234,534,5134]
+// Function to separate odd & even numbers
+function oddOrEven(a) {
+  // Loop through array
+  for (let i = 0; i < a.length; i++) {
 
-
-function oddOrEven (a){
-  for( let i = 0 ; i <= a.length ; i++ ){
-  if(a[i] % 2 == 0){
-    evenNums.push(a[i])
+    // If number is even
+    if (a[i] % 2 === 0) {
+      evenNums.push(a[i]); // add to even array
+    } 
+    // If number is odd
+    else {
+      oddNums.push(a[i]); // add to odd array
+    }
   }
-  else{
-    oddNums.push(a[i])
-  }
-  
-}
-  
 }
 
-oddOrEven(groupofNums)
-console.log(evenNums)
-console.log(oddNums)
+// First run
+oddOrEven(groupofNums);
+console.log(evenNums); // evens from groupofNums
+console.log(oddNums);  // odds from groupofNums
 
-oddOrEven(groupofNums2)
+// Second run (arrays KEEP growing)
+oddOrEven(groupofNums2);
+console.log(evenNums); // evens from BOTH arrays
+console.log(oddNums);  // odds from BOTH arrays
 
-console.log(evenNums)
-console.log(oddNums)
+// Filter original array using evenNums
+let evenNumbers = groupofNums.filter(num => evenNums.includes(num));
 
- let oddNumbers = groupofNums.filter((groupofNum) => {
-   return groupofNums = groupofNum.pop(evenNums)
- })
- 
- console.log(oddNumbers)
+// Anything not in evenNums is odd
+let oddNumbers = groupofNums.filter(num => !evenNums.includes(num));
+
+console.log(oddNumbers);
+console.log(evenNumbers);
