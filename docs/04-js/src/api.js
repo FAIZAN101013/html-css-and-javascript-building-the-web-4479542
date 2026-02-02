d20d45aab05ed389145427fd7a1924fd2ff05701
FAIZAN PATEL <68345709+FAIZAN101013@users.noreply.github.com>
2026-02-02 10:03:40 +0000

    working on apis

---
+++ docs/04-js/src/api.js
@@ -0,0 +1,17 @@
+// const apiData = async () => {
+//   try{
+//     const url = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const data = await url.json();
+//      data.forEach(user => {
+//       console.log(user.name);
+//     })
+//   }catch(err){
+//     console.log("Error")
+//   }
+// } 
+
+//apiData()
+
+
+// I am trying call an aysn function for api
+
